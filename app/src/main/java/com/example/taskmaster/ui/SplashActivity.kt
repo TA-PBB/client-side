174ccdf23f75fa14631c97997075f5c4c9fab702
Abdullahhasbi26 <hasbiabdullah279@gmail.com>
2024-06-07 20:57:50 +0700

    Splash Activity Fix

--- app/src/main/java/com/example/taskmaster/ui/SplashActivity.kt
+++ app/src/main/java/com/example/taskmaster/ui/SplashActivity.kt
@@ -18,6 +18,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // Delay in milliseconds (e.g., 3000ms = 3 seconds)
+        }, 1000) // Delay in milliseconds (e.g., 3000ms = 3 seconds)
     }
 }
