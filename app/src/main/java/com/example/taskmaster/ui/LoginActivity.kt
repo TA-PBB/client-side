e8282f0f58046b79d1125065bb4bb7039492c30e
Slamet Ispriyadi <ispriyadi04@gmail.com>
2024-06-14 22:39:41 +0700

    Frondtend LOGIN

--- app/src/main/java/com/example/taskmaster/ui/LoginActivity.kt
+++ app/src/main/java/com/example/taskmaster/ui/LoginActivity.kt
@@ -1,12 +1,46 @@
 package com.example.taskmaster.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.taskmaster.R
 
 class LoginActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val etUsername = findViewById<EditText>(R.id.et_username)
+        val etPassword = findViewById<EditText>(R.id.et_password)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        val tvRegisterNow = findViewById<TextView>(R.id.tv_register_now)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                // Lakukan tindakan login (misalnya, autentikasi)
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        tvRegisterNow.setOnClickListener {
+            // Arahkan ke aktivitas pendaftaran
+            Toast.makeText(this, "Register Now clicked", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
