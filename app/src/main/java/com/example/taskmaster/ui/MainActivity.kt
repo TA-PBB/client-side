b7d9e44a4e781949a65d66cf7599a52254120cfd
syasul <githubku4@gmail.com>
2024-06-23 01:37:32 +0700

    feat(FE): fix error auth

--- app/src/main/java/com/example/taskmaster/ui/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import com.example.mykotlinclientapp.api.ApiClient
@@ -16,7 +17,7 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var logoutButton: Button
+    private lateinit var logoutButton: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
